f7ea588bc0e5e799ecd75902ae2e280fab556df7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-07 18:38:33 +0200

    1. ejercicioudp00: declaro los objetos de tipo DatagramSocket para enviar y recibir información

--- src/main/java/com/susanafigueroa/ejercicioudp00/Cliente.java
+++ src/main/java/com/susanafigueroa/ejercicioudp00/Cliente.java
@@ -1,7 +1,11 @@
 package com.susanafigueroa.ejercicioudp00;
 
+import java.net.DatagramSocket;
+
 public class Cliente {
 
+    // para que el cliente reciba y envíe paquetes
+    private DatagramSocket clientSocket;
 
     // main para ejecutar el cliente
     public static void main(String[] args) {
--- src/main/java/com/susanafigueroa/ejercicioudp00/Servidor.java
+++ src/main/java/com/susanafigueroa/ejercicioudp00/Servidor.java
@@ -1,6 +1,11 @@
 package com.susanafigueroa.ejercicioudp00;
 
+import java.net.DatagramSocket;
+
 public class Servidor {
+
+    // para que el servidor reciba y envíe paquetes
+    private DatagramSocket serverSocket;
 
     // main para ejecutar el servidor
     public static void main(String[] args) {
