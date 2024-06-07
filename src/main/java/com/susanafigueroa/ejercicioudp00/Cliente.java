fe09e83c92878e654618520435bb24963b7b72c1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-07 19:04:10 +0200

    2. ejercicioudp00: creación de los métodos init para inicializar variables

--- src/main/java/com/susanafigueroa/ejercicioudp00/Cliente.java
+++ src/main/java/com/susanafigueroa/ejercicioudp00/Cliente.java
@@ -1,11 +1,29 @@
 package com.susanafigueroa.ejercicioudp00;
 
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Cliente {
 
-    // para que el cliente reciba y envíe paquetes
     private DatagramSocket clientSocket;
+    private InetAddress ipServidor;
+    private int puertoServidor;
+
+    public void init(String host, int port) throws SocketException, UnknownHostException {
+        // inicializo el clientSocket para que el cliente pueda enviar datagramas al servidor
+        // y pueda recibir datagramas del servidor
+        clientSocket = new DatagramSocket();
+
+        // inicializo la IP del servidor para que el cliente pueda enviar datagramas al servidor
+        // y pueda recibir datagramas del servidor
+        ipServidor = InetAddress.getByName(host);
+
+        // puerto del servidor para que el cliente pueda enviar datagramas al servidor
+        // y pueda recibir datagramas del servidor
+        puertoServidor = port;
+    }
 
     // main para ejecutar el cliente
     public static void main(String[] args) {
--- src/main/java/com/susanafigueroa/ejercicioudp00/Servidor.java
+++ src/main/java/com/susanafigueroa/ejercicioudp00/Servidor.java
@@ -1,11 +1,17 @@
 package com.susanafigueroa.ejercicioudp00;
 
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class Servidor {
 
-    // para que el servidor reciba y envíe paquetes
     private DatagramSocket serverSocket;
+
+    public void init(int puertoCliente) throws SocketException {
+        // inicializo el serverSocket para poder enviar datagramas al cliente y recibir datagramas
+        // del cliente
+        serverSocket = new DatagramSocket(puertoCliente);
+    }
 
     // main para ejecutar el servidor
     public static void main(String[] args) {
