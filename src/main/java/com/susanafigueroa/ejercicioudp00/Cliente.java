c7363b263b41cf00b35488145300674d7eda23fb
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-06-07 18:28:00 +0200

    0. ejercicioudp00: añado las clases Cliente y Servidor

---
+++ src/main/java/com/susanafigueroa/ejercicioudp00/Cliente.java
@@ -0,0 +1,10 @@
+package com.susanafigueroa.ejercicioudp00;
+
+public class Cliente {
+
+
+    // main para ejecutar el cliente
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/susanafigueroa/ejercicioudp00/Servidor.java
@@ -0,0 +1,9 @@
+package com.susanafigueroa.ejercicioudp00;
+
+public class Servidor {
+
+    // main para ejecutar el servidor
+    public static void main(String[] args) {
+
+    }
+}
